17dcbda6e27fb7f220943b8ae9c70aef2af3d4a6
MWCheb <marcin.wardak.cheb@outlook.com>
2025-04-11 10:01:26 +0000

    Comment out HOST and PORT in DATABASES configuration

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'octofit_db',
-        'HOST': 'localhost',
-        'PORT': 27017,  # Default MongoDB port
+        # 'HOST': 'localhost',
+        # 'PORT': 27017,  # Default MongoDB port
     }
 }
 
